4cfbecc6305e5d92877959d2e0e96b63a2ff8a8d
prscalassara <106551221+prscalassara@users.noreply.github.com>
2024-08-23 18:24:53 -0300

    Update ex01.asm

--- ex01/ex01.asm
+++ ex01/ex01.asm
@@ -1,53 +1,53 @@
 ; ET37I - Sistemas Microcontrolados 
-; Exemplo 1 - Bot„o e Led (Assembly) PICSimLab
+; Exemplo 1 - Bot√£o e Led (Assembly) PICSimLab
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; *                  CONFIGURA«’ES		         				  *
+; *                  CONFIGURA√á√ïES				  *
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; Deve-se iniciar a linha abaixo com tabulaÁ„o
+; Deve-se iniciar a linha abaixo com tabula√ß√£o
 	CONFIG  FOSC = HS, WDT = OFF, PBADEN = OFF, LVP = OFF
 ; FOSC = HS		Oscilador de 20MHz -> Tcy = 200ns
 ; WDT = OFF		WDT desabilitado
-; PBADEN = OFF	Pinos de PORTB [4:0] como I/O digital
-; LVP = OFF		GravaÁ„o em modo de baixa tens„o desabilitada
+; PBADEN = OFF		Pinos de PORTB [4:0] como I/O digital
+; LVP = OFF		Grava√ß√£o em modo de baixa tens√£o desabilitada
 
 #INCLUDE <P18F4550.INC> ; MICROCONTROLADOR PIC18F4550
 
-#DEFINE	BOTAO	PORTB,1	; PINO DO BOT√O
-#DEFINE	LED		PORTD,0	; PINO DO LED
+#DEFINE	BOTAO	PORTB,1	; PINO DO BOT√ÉO
+#DEFINE	LED	PORTD,0	; PINO DO LED
 
-	ORG	0x00			; VETOR DE RESET
+	ORG	0x00	; VETOR DE RESET
 	GOTO	INICIO
 	
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-;*                     INICIO DO PROGRAMA             			   *
+;*                     INICIO DO PROGRAMA         		   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 INICIO
 	MOVLW	B'00000010'	; DEFINE RB1 COMO ENTRADA E
-	MOVWF	TRISB		; O RESTO DO PORTB COMO SAÕDA
+	MOVWF	TRISB		; O RESTO DO PORTB COMO SA√çDA
 	MOVLW	B'00000000'
-	MOVWF	TRISD		; DEFINE TODO O PORTD COMO SAÕDA
+	MOVWF	TRISD		; DEFINE TODO O PORTD COMO SA√çDA
 	
 	MOVLW	B'00000000'
 	MOVWF	INTCON2		; PULL-UPS HABILITADOS
 	MOVLW	B'00000000'
-	MOVWF	INTCON		; TODAS AS INTERRUP«’ES DESLIGADAS
+	MOVWF	INTCON		; TODAS AS INTERRUP√á√ïES DESLIGADAS
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ;*                     ROTINA PRINCIPAL                      	   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 MAIN_LOOP
-	BTFSC	BOTAO		; O BOT√O EST¡ PRESSIONADO?
-	BRA		BOTAO_LIB	; N√O, ENT√O TRATA BOT√O LIBERADO
-	BRA		BOTAO_PRES	; SIM, ENT√O TRATA BOT√O PRESSIONADO
+	BTFSC	BOTAO		; O BOT√ÉO EST√Å PRESSIONADO?
+	BRA	BOTAO_LIB	; N√ÉO, ENT√ÉO TRATA BOT√ÉO LIBERADO
+	BRA	BOTAO_PRES	; SIM, ENT√ÉO TRATA BOT√ÉO PRESSIONADO
 
 BOTAO_LIB
-	BCF		LED			; APAGA O LED
+	BCF	LED		; APAGA O LED
 	BRA 	MAIN_LOOP	; RETORNA AO LOOP PRINCIPAL
 
 BOTAO_PRES
-	BSF		LED			; ACENDE O LED
+	BSF	LED		; ACENDE O LED
 	BRA 	MAIN_LOOP	; RETORNA AO LOOP PRINCIPAL
 
-	END					; FIM DO PROGRAMA
+	END			; FIM DO PROGRAMA
 
