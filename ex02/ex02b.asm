11a948a824a5cbc1c9a6588b862ebbd3fa72520e
prscalassara <106551221+prscalassara@users.noreply.github.com>
2024-09-04 12:17:51 -0300

    Update ex02b.asm

--- ex02/ex02b.asm
+++ ex02/ex02b.asm
@@ -1,41 +1,41 @@
 ; ET37I - Sistemas Microcontrolados
 ; Ex. 2 - Contador com leds (Assembly) PICSimLab
-; 	(b)Utilizando atraso no bot„o
+; 	(b)Utilizando atraso no bot√£o
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; *                  CONFIGURA«’ES					                *
+; *                  CONFIGURA√á√ïES				    *
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; Deve-se iniciar a linha abaixo com tabulaÁ„o
+; Deve-se iniciar a linha abaixo com tabula√ß√£o
 	CONFIG  FOSC = HS, CPUDIV = OSC1_PLL2, WDT = OFF, PBADEN = OFF, LVP = OFF
 ; FOSC = HS		Oscilador de 20MHz -> Tcy = 200ns
 ; WDT = OFF		WDT desabilitado
-; PBADEN = OFF	Pinos de PORTB [4:0] como I/O digital
-; LVP = OFF		GravaÁ„o em modo de baixa tens„o desabilitada
+; PBADEN = OFF		Pinos de PORTB [4:0] como I/O digital
+; LVP = OFF		Grava√ß√£o em modo de baixa tens√£o desabilitada
 
 #INCLUDE <P18F4550.INC> 
 
     CBLOCK  0X00   
-		CONTADOR		; ARMAZENA O VALOR DA CONTAGEM
-		ATRASO			; VARI¡VEL DE ATRASO PARA O BOT√O
+	CONTADOR		; ARMAZENA O VALOR DA CONTAGEM
+	ATRASO			; VARI√ÅVEL DE ATRASO PARA O BOT√ÉO
     ENDC
 
 ; CONSTANTES
-TEMPO_ATRASO	EQU	.250	; ATRASO PARA BOT√O
+TEMPO_ATRASO	EQU	.250	; ATRASO PARA BOT√ÉO
 
-#DEFINE	BOTAO_1	PORTB,0	; PINO DO BOT√O_1
+#DEFINE	BOTAO_1	PORTB,0	; PINO DO BOT√ÉO_1
 
 	ORG	0x00			
 	GOTO	INICIO
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-;*                     INICIO DO PROGRAMA                 		   *
+;*                     INICIO DO PROGRAMA              		   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 INICIO
 	MOVLW	B'00000001'
 	MOVWF	TRISB		; DEFINE RB0 COMO ENTRADA
-	CLRF	TRISD		; DEFINE TODO O PORTD COMO SAÕDA
+	CLRF	TRISD		; DEFINE TODO O PORTD COMO SA√çDA
 	CLRF	INTCON2		; PULL-UPS HABILITADOS
-	CLRF	INTCON		; TODAS AS INTERRUP«’ES DESLIGADAS
+	CLRF	INTCON		; TODAS AS INTERRUP√á√ïES DESLIGADAS
 	
 	CLRF	LATD		; INICIA PORTD COM LEDS APAGADOS
 	CLRF	CONTADOR	; INICIA CONTADOR = 0
@@ -48,20 +48,20 @@ MAIN
 	MOVWF	ATRASO		; INICIALIZA ATRASO = TEMPO_ATRASO
 
 VERIFICA_BT1
-	BTFSC	BOTAO_1		; O BOT√O_1 EST¡ PRESSIONADO?
-	BRA		MAIN		; N√O, VOLTA
-						; SIM
-	DECFSZ	ATRASO,F	; DECREMENTA O ATRASO DO BOT√O
-						; TERMINOU?
-	BRA		VERIFICA_BT1; N√O, CONTINUA ESPERANDO
-						; SIM
+	BTFSC	BOTAO_1		; O BOT√ÉO_1 EST√Å PRESSIONADO?
+	BRA	MAIN		; N√ÉO, VOLTA
+				; SIM
+	DECFSZ	ATRASO,F	; DECREMENTA O ATRASO DO BOT√ÉO
+				; TERMINOU?
+	BRA	VERIFICA_BT1	; N√ÉO, CONTINUA ESPERANDO
+				; SIM
 	INCF	CONTADOR,F	; INCREMENTA O CONTADOR
 	MOVF	CONTADOR,W	; COLOCA CONTADOR EM W
 	MOVWF	LATD		; ATUALIZA O PORTD
 
 ESPERA_BT1
-	BTFSS	BOTAO_1		; O BOT√O CONTINUA PRESSIONADO?
-	BRA		ESPERA_BT1	; SIM, ENT√O ESPERA LIBERA«√O
-	BRA		MAIN		; N√O, VOLTA AO LOOP PRINCIPAL
+	BTFSS	BOTAO_1		; O BOT√ÉO CONTINUA PRESSIONADO?
+	BRA	ESPERA_BT1	; SIM, ENT√ÉO ESPERA LIBERA√á√ÉO
+	BRA	MAIN		; N√ÉO, VOLTA AO LOOP PRINCIPAL
 
-	END					; FIM DO PROGRAMA
+	END			; FIM DO PROGRAMA
