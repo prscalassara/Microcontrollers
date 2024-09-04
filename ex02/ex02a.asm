2a92690fad14ea413680824d68a4f7555ce04400
prscalassara <106551221+prscalassara@users.noreply.github.com>
2024-09-04 11:52:01 -0300

    Add files via upload

--- ex02/ex02a.asm
+++ ex02/ex02a.asm
@@ -1,57 +1,58 @@
 ; ET37I - Sistemas Microcontrolados
 ; Ex. 2 - Contador com leds (Assembly) PICSimLab
-; (a) Pressionando e liberando o bot√£o
+; (a) Pressionando e liberando o bot„o
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; *                  CONFIGURA√á√ïES			 	    *
+; *                  CONFIGURA«’ES			 						*
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; Deve-se iniciar a linha abaixo com tabula√ß√£o
-    CONFIG  FOSC = HS, WDT = OFF, PBADEN = OFF, LVP = OFF
-; FOSC = HS             Oscilador de 20MHz -> Tcy = 200ns
-; WDT = OFF             WDT desabilitado
-; PBADEN = OFF          Pinos de PORTB [4:0] como I/O digital
-; LVP = OFF             Grava√ß√£o em modo de baixa tens√£o desabilitada
+; Deve-se iniciar a linha abaixo com tabulaÁ„o
+	CONFIG  FOSC = HS, WDT = OFF, PBADEN = OFF, LVP = OFF
+; FOSC = HS		Oscilador de 20MHz -> Tcy = 200ns
+; WDT = OFF		WDT desabilitado
+; PBADEN = OFF	Pinos de PORTB [4:0] como I/O digital
+; LVP = OFF		GravaÁ„o em modo de baixa tens„o desabilitada
 
 #INCLUDE <P18F4550.INC> 
 
-; VARI√ÅVEIS
-        CBLOCK  0X00                ; BANCO DE ACESSO
-        	CONTADOR		; ARMAZENA O VALOR DA CONTAGEM
-        ENDC
+; VARI¡VEIS
+    CBLOCK  0X00    	; BANCO DE ACESSO
+		CONTADOR		; ARMAZENA O VALOR DA CONTAGEM
+    ENDC
 
-#DEFINE	BOTAO_1	PORTB,0	; PINO DO BOT√ÉO_1
+#DEFINE	BOTAO_1	PORTB,0	; PINO DO BOT√O_1
 
-        ORG	0x00			; ENDERE√áO INICIAL DO PROGRAMA
-        GOTO	INICIO
+	ORG	0x00			; ENDERE«O INICIAL DO PROGRAMA
+	GOTO	INICIO
 	
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-;*                     INICIO DO PROGRAMA                 	   *
+;*                     INICIO DO PROGRAMA                 		   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 INICIO
-        MOVLW	B'00000001'
-        MOVWF	TRISB
-        CLRF	TRISD		; DEFINE TODO O PORTD COMO SA√çDA
-        CLRF	INTCON2		; PULL-UPS HABILITADOS
-        CLRF	INTCON		; TODAS AS INTERRUP√á√ïES DESLIGADAS
-        
-        CLRF	LATD		; INICIA PORTD COM LEDS APAGADOS
-        CLRF	CONTADOR	; INICIA CONTADOR = 0
+	MOVLW	B'00000001'
+	MOVWF	TRISB
+	CLRF	TRISD		; DEFINE TODO O PORTD COMO SAÕDA
+	CLRF	INTCON2		; PULL-UPS HABILITADOS
+	CLRF	INTCON		; TODAS AS INTERRUP«’ES DESLIGADAS
+	
+	CLRF	LATD		; INICIA PORTD COM LEDS APAGADOS
+	CLRF	CONTADOR	; INICIA CONTADOR = 0
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ;*                     ROTINA PRINCIPAL                      	   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 MAIN
-        BTFSC	BOTAO_1		; O BOT√ÉO_1 EST√Å PRESSIONADO?
-        BRA	MAIN		; N√ÉO, VOLTA
-				; SIM
-        INCF	CONTADOR,F	; INCREMENTA O CONTADOR
-        MOVF	CONTADOR,W	; COLOCA CONTADOR EM W
-        MOVWF	LATD		; ATUALIZA O PORTD
+	BTFSC	BOTAO_1		; O BOT√O_1 EST¡ PRESSIONADO?
+	BRA		MAIN		; N√O, VOLTA
+						; SIM
+	INCF	CONTADOR,F	; INCREMENTA O CONTADOR
+	MOVF	CONTADOR,W	; COLOCA CONTADOR EM W
+	MOVWF	LATD		; ATUALIZA O PORTD
 
 ESPERA
-        BTFSS	BOTAO_1		; O BOT√ÉO CONTINUA PRESSIONADO?
-        BRA	ESPERA		; SIM, ENT√ÉO ESPERA LIBERA√á√ÉO
-        BRA	MAIN		; N√ÉO, VOLTA AO LOOP PRINCIPAL
+	BTFSS	BOTAO_1		; O BOT√O CONTINUA PRESSIONADO?
+	BRA		ESPERA		; SIM, ENT√O ESPERA LIBERA«√O
+	BRA		MAIN		; N√O, VOLTA AO LOOP PRINCIPAL
 
-        END			; FIM DO PROGRAMA
+	END					; FIM DO PROGRAMA
+
--- ex02/ex02b.asm
+++ ex02/ex02b.asm
@@ -1,73 +1,67 @@
 ; ET37I - Sistemas Microcontrolados
-; Ex. 2 - Contador com leds (Assembly)
+; Ex. 2 - Contador com leds (Assembly) PICSimLab
 ; 	(b)Utilizando atraso no botão
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ; *                  CONFIGURAÇÕES					                *
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ; Deve-se iniciar a linha abaixo com tabulação
-	CONFIG  FOSC = HS, WDT = OFF, PBADEN = OFF, LVP = OFF, DEBUG = ON
-; FOSC = HS: Oscilador de 20MHz -> Tcy = 200ns
-; WDT = OFF: WDT desabilitado
-; PBADEN = OFF:	Pinos de PORTB [4:0] configurados como I/O digital. 
-; LVP = OFF: Gravação em modo de baixa tensão desabilitada
-; DEBUG = ON: Depurador habilitado, RB6 and RB7 são dedicados a depuração in-circuit
+	CONFIG  FOSC = HS, CPUDIV = OSC1_PLL2, WDT = OFF, PBADEN = OFF, LVP = OFF
+; FOSC = HS		Oscilador de 20MHz -> Tcy = 200ns
+; WDT = OFF		WDT desabilitado
+; PBADEN = OFF	Pinos de PORTB [4:0] como I/O digital
+; LVP = OFF		Gravação em modo de baixa tensão desabilitada
 
- #INCLUDE <P18F4550.INC> ; MICROCONTROLADOR UTILIZADO
+#INCLUDE <P18F4550.INC> 
 
-; VARIÁVEIS
-    CBLOCK  0X00    ;ACCESS BANK
-		CONTADOR	;ARMAZENA O VALOR DA CONTAGEM
-		ATRASO		;VARIÁVEL DE ATRASO PARA O BOTÃO
+    CBLOCK  0X00   
+		CONTADOR		; ARMAZENA O VALOR DA CONTAGEM
+		ATRASO			; VARIÁVEL DE ATRASO PARA O BOTÃO
     ENDC
 
 ; CONSTANTES
-TEMPO_ATRASO	EQU	.250	;ATRASO PARA BOTÃO
+TEMPO_ATRASO	EQU	.250	; ATRASO PARA BOTÃO
 
-#DEFINE	BOTAO_1	PORTB,0	;PINO DO BOTÃO_1
+#DEFINE	BOTAO_1	PORTB,0	; PINO DO BOTÃO_1
 
-	ORG	0x00			;ENDEREÇO INICIAL DO PROGRAMA
+	ORG	0x00			
 	GOTO	INICIO
-	
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-;*                     INICIO DO PROGRAMA                          *
+;*                     INICIO DO PROGRAMA                 		   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 INICIO
 	MOVLW	B'00000001'
-	MOVWF	TRISB		;DEFINE RB0 COMO ENTRADA
-	MOVLW	B'00000000'
-	MOVWF	TRISD		;DEFINE TODO O PORTD COMO SAÍDA
-	CLRF	INTCON2		;PULL-UPS HABILITADOS
-
-	MOVLW	B'00000000'
-	MOVWF	INTCON		;TODAS AS INTERRUPÇÕES DESLIGADAS
+	MOVWF	TRISB		; DEFINE RB0 COMO ENTRADA
+	CLRF	TRISD		; DEFINE TODO O PORTD COMO SAÍDA
+	CLRF	INTCON2		; PULL-UPS HABILITADOS
+	CLRF	INTCON		; TODAS AS INTERRUPÇÕES DESLIGADAS
 	
-	SETF	LATD		;INICIA PORTD COM LEDS APAGADOS
-	CLRF	CONTADOR	;INICIA CONTADOR = 0
+	CLRF	LATD		; INICIA PORTD COM LEDS APAGADOS
+	CLRF	CONTADOR	; INICIA CONTADOR = 0
 
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-;*                     ROTINA PRINCIPAL                            *
+;*                     ROTINA PRINCIPAL                      	   *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 MAIN
 	MOVLW	TEMPO_ATRASO
-	MOVWF	ATRASO		;INICIALIZA ATRASO = TEMPO_ATRASO
+	MOVWF	ATRASO		; INICIALIZA ATRASO = TEMPO_ATRASO
 
 VERIFICA_BT1
-	BTFSC	BOTAO_1		;O BOTÃO_1 ESTÁ PRESSIONADO?
-	BRA		MAIN		;NÃO, VOLTA
-						;SIM
-	DECFSZ	ATRASO,F	;DECREMENTA O ATRASO DO BOTÃO
-						;TERMINOU?
-	BRA		VERIFICA_BT1;NÃO, CONTINUA ESPERANDO
-						;SIM
-	INCF	CONTADOR,F	;INCREMENTA O CONTADOR
-	COMF	CONTADOR,W	;INVERTE O CONTADOR E COLOCA EM W
-	MOVWF	LATD		;ATUALIZA O PORTD
+	BTFSC	BOTAO_1		; O BOTÃO_1 ESTÁ PRESSIONADO?
+	BRA		MAIN		; NÃO, VOLTA
+						; SIM
+	DECFSZ	ATRASO,F	; DECREMENTA O ATRASO DO BOTÃO
+						; TERMINOU?
+	BRA		VERIFICA_BT1; NÃO, CONTINUA ESPERANDO
+						; SIM
+	INCF	CONTADOR,F	; INCREMENTA O CONTADOR
+	MOVF	CONTADOR,W	; COLOCA CONTADOR EM W
+	MOVWF	LATD		; ATUALIZA O PORTD
 
 ESPERA_BT1
-	BTFSS	BOTAO_1		;O BOTÃO CONTINUA PRESSIONADO?
-	BRA		ESPERA_BT1	;SIM, ENTÃO ESPERA LIBERAÇÃO
-	BRA		MAIN		;NÃO, VOLTA AO LOOP PRINCIPAL
+	BTFSS	BOTAO_1		; O BOTÃO CONTINUA PRESSIONADO?
+	BRA		ESPERA_BT1	; SIM, ENTÃO ESPERA LIBERAÇÃO
+	BRA		MAIN		; NÃO, VOLTA AO LOOP PRINCIPAL
 
-	END					;FIM DO PROGRAMA
+	END					; FIM DO PROGRAMA
